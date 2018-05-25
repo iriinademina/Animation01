1a9ca5a151b347c7401ec90153821ce59702150e
Irina <iriinademina@gmail.com>
2018-05-25 23:35:37 +0300

    Change buttons

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ document.routePoints = [
     [ 50, 50 ], [ 350, 80 ], [ 580, 190 ], [ 160, 320 ]
 ]
 
-document.pointImage="./images/christmas-tree-lights-animated.gif"
+document.pointPicture="./images/christmas-tree-lights-animated.gif"
 
 
 var Personage = function ( params ) {
@@ -17,7 +17,7 @@ var Personage = function ( params ) {
                         ? this.defaultPointImage
                         : params.pointImage
     this.route =  !params || !params.routePoints ||
-                  !Array.isArray ( params.routePoints ) ?
+    						  !Array.isArray ( params.routePoints ) ?
                   this.defaultRoute : params.routePoints
     this.setRoutePoints ()
     this.nextPoint = 1
@@ -28,26 +28,37 @@ var Personage = function ( params ) {
     this.delay = !params || !params.timeInterval
                          || typeof params.timeInterval !== "number"
                          ? 100 : params.timeInterval
-   this.interval = setInterval ( this.mc_personage.bind ( this ), this.delay )
+    this.startMove = function(){
+          this.interval = setInterval ( this.mc_personage.bind ( this ), this.delay )}
 
-   this.stopMove = function(){
-        clearInterval (this.interval )}.bind ( this )
-}
+    this.stopMove = function () {   // метод для остановки движения
+        clearInterval ( this.interval )
+    }.bind ( this )
+
+   }
+
+
 
 Personage.prototype.setRoutePoints = function () {
     for ( var item of this.route ) {
-        if ( !Array.isArray ( item ) ||
-              typeof item [0] !== 'number' ||
+    		if ( !Array.isArray ( item ) ||
+        			typeof item [0] !== 'number' ||
               typeof item [1] !== 'number'
         ) continue
         var point = document.createElement ( 'figure' )
-        this.elementStyling ( point,
+
+          point.backgroundImage =  function(){
+              this.defaultPointImage.forEach(function(item){
+                          "url(" + item + ")"})}
+
+       this.elementStyling ( point,
                 Object.assign ( this.pointStyle, {
                     left: item [0] + 'px',
                     top:  item [1] + 'px',
                     backgroundImage: "url(" + this.pointImage + ")"
                 } )
         )
+
         document.body.appendChild ( point )
     }
 }
@@ -89,21 +100,21 @@ Personage.prototype.mc_personage = function ( event ) {
 }
 
 Personage.prototype.defaultRoute = [
-    [ 50, 50 ], [ 300, 300 ], [ 100, 300 ], [ 200, 50 ]
+		[ 50, 50 ], [ 300, 300 ], [ 100, 300 ], [ 200, 50 ]
 ]
 Personage.prototype.defaultPersonageImage = "./images/3-santa-sleigh-reindeer.gif"
 Personage.prototype.defaultPointImage = "./images/christmas-tree-white-lights.gif"
 
 
 Personage.prototype.personageStyle = {
-    position: "fixed",
+		position: "fixed",
     top: 0,
     left: 0,
     width: "90px",
     height: "auto"
 }
 Personage.prototype.pointStyle = {
-    position: "fixed",
+		position: "fixed",
     top: 0,
     left: 0,
     width: "120px",
@@ -111,18 +122,29 @@ Personage.prototype.pointStyle = {
     backgroundRepeat: "no-repeat",
     backgroundSize: "contain",
     backgroundPosition: "center center",
-    backgroundImage: "url(" + this.defaultPointImage + ")"
+backgroundImage: "url(" + this.defaultPointImage + ")"
 }
 
 document.personage = new Personage ( {
       routePoints: document.routePoints,
-      pointImage: document.pointImage // добавлена другая картинка
+      pointImage: document.pointPicture // добавлена другая картинка
+
 })
+
+var buttonStart = document.createElement('button')
+buttonStart.innerHTML = "START"
+document.body.appendChild(buttonStart)
+buttonStart.onclick = function (event) {
+        document.personage.startMove()
+}
 
 var buttonStop = document.createElement('button')
 buttonStop.innerHTML = "STOP"
 document.body.appendChild(buttonStop)
 buttonStop.onclick= function (event) {
+        document.personage.stopMove()
+}
+
         document.personage.stopMove()
 }
 
